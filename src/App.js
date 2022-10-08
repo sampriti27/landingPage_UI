e94a076f2be2986ec3ae1d98d28fc3f371d82da7
Amrit Raj <77192710+amritmaurya1504@users.noreply.github.com>
2022-10-08 13:00:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -134,20 +134,7 @@ export default function App() {
 
               <Disclosure.Panel className="md:hidden">
                 <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                  {navigation.map((item) => (
-                    <Disclosure.Button
-                      key={item.name}
-                      as="a"
-                      href={item.href}
-                      className={classNames(
-                        item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                        'block px-3 py-2 rounded-md text-base font-medium'
-                      )}
-                      aria-current={item.current ? 'page' : undefined}
-                    >
-                      {item.name}
-                    </Disclosure.Button>
-                  ))}
+                  // render navigation
                 </div>
                 <div className="pt-4 pb-3 border-t border-gray-900">
                   <div className="flex items-center px-5">
